63851e36ea8bcf38cd95e170193bb8d7aadccef5
brandorMcD <222bmcdonald@gmail.com>
2017-02-21 19:04:12 -0800

    the first commit

---
+++ The Tale of Bebito Stevens/Assets/Scripts/charactermovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class charactermovement : MonoBehaviour {
+	
+    private float movex;
+    private float movey;
+	public float speed;
+    Animator charmovement;
+
+	// Use this for initialization
+	void Start ()
+    {
+        charmovement = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        movex = Input.GetAxis("Horizontal");
+        movey = Input.GetAxis("Vertical");
+
+        if (movex < 0)
+        {
+            charmovement.SetInteger("CharacterState", 4);//MoveLeft
+        }
+        else if (movex > 0)
+        {
+            charmovement.SetInteger("CharacterState", 3);//MoveRight
+        }
+        else if (movey > 0)
+        {
+            charmovement.SetInteger("CharacterState", 2);//MoveUp
+        }
+        else if (movey < 0)
+        {
+            charmovement.SetInteger("CharacterState", 1);//MoveDown
+        }
+        else
+        {
+            charmovement.SetInteger("CharacterState", 0);//Idle
+        }
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(movex * speed, movey * speed);
+
+    }
+}
